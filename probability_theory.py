c3f384016acef997801a6d085eeeaeb7afa8c5bc
taylorturton <126579693+taylorturton@users.noreply.github.com>
2023-10-09 11:43:34 +0100

    Add files via upload

---
+++ probability_theory.py
@@ -0,0 +1,11 @@
+# Total number of cards in a deck
+total_cards = 52
+
+# Number of aces in a deck
+aces = 4
+
+# Probability of drawing an ace
+probability = aces / total_cards
+
+# Print the result
+print("Probability of drawing an ace:", probability)
